79c587933cabbc3290e85a7ba87fb1352011185b
Oluwatobi Alese <tobillionaire95@gmail.com>
2022-05-14 05:10:22 -0400

    Breadecrumbs SubHeader

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header';
-import CampsitesList from './features/campsites/CampsitesList';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-            <Header />
-            <CampsitesList />
-            <Footer/>
-    </div>
-  );
+    return (
+      <div className='App'>
+              <Header />
+              <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='contact' element={<ContactPage />} />
+                    <Route path='directory' element={<CampsitesDirectoryPage />} />
+                    <Route
+                        path='directory/:campsiteId'
+                        element={<CampsiteDetailPage />}
+                    />
+              </Routes>
+              <Footer/>
+        </div>
+    );
 }
 
 export default App;
