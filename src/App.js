48ddf10906712edf4ba899f3d1230e7f30eac7e9
Oluwatobi Alese <tobillionaire95@gmail.com>
2022-05-16 11:18:45 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 
@@ -20,6 +21,7 @@ function App() {
                         path='directory/:campsiteId'
                         element={<CampsiteDetailPage />}
                     />
+                    <Route path='about' element={<AboutPage />} />
               </Routes>
               <Footer/>
         </div>
